6a0bf6d39cca215ee847dab89a5c41015b9c31b6
aleixfortm <aleix.fort45@gmail.com>
2023-06-14 17:57:50 +0200

    fads

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production'
-  ? '/storymous-migration/'
-  : '/'
+  publicPath: '/storymous-migration/'
+
 })
