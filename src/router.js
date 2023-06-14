96bce9bb2823019a1d2f5d70dd3be6114ddc5c7e
aleixfortm <aleix.fort45@gmail.com>
2023-06-14 23:49:20 +0200

    asdfdsa

--- src/router.js
+++ src/router.js
@@ -3,16 +3,16 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomePage from "./pages/HomePage.vue";
 import PostPage from "./pages/PostPage.vue";
 import UserPage from "./pages/UserPage.vue";
-import NotfoundPage from "./pages/NotfoundPage.vue";
+//import NotfoundPage from "./pages/NotfoundPage.vue";
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: "/storymous-migration", redirect: "/storymous-migration/home" },
         { path: "/storymous-migration/home", component: HomePage },
-        { path: "/user/:id", component: UserPage },
-        { path: "/post/:id", component: PostPage },
-        { path: "/:notFound(.*)", component: NotfoundPage } // regular expression for anything entered
+        { path: "/storymous-migration/user/:id", component: UserPage },
+        { path: "/storymous-migration/post/:id", component: PostPage },
+        { path: "/storymous-migration/:notFound(.*)", component: HomePage } // regular expression for anything entered
     ],
 });
 
