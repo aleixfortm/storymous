d59d875a36067a0f948044c05c3db3a4963ed790
aleixfortm <aleix.fort45@gmail.com>
2023-06-15 11:34:50 +0200

    asdf

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === 'production' ? '/storymous-migration/dist' : '/dist',
-  outputDir: 'dist',
+  outputDir: '/',
 })
