0af7fa17e5b4f48f973f058c32e1866cb2658cc2
aleixfortm <aleix.fort45@gmail.com>
2023-06-22 12:18:20 +0200

    changes

--- src/components/TopMessage.vue
+++ src/components/TopMessage.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="message">
-        Welcome to Storymous! <br> This site is in early development and all of its content is static. <br><u> Feel free to log in with random credentials and
+        This site is in early development and all of its content is static. <br><u> Feel free to log in with random credentials and
         check it out. </u>
     </div>
 </template>
