373cb4b41b5d5f664a22a1298438275416be2d29
aleixfortm <aleix.fort45@gmail.com>
2023-06-24 23:45:58 +0200

    added no following posts

--- src/pages/subpages/FollowingFeed.vue
+++ src/pages/subpages/FollowingFeed.vue
@@ -1,17 +1,26 @@
 <template>
     <feed-container>
-        <post-container
-            v-for="post in posts.following"
-            :key="post._id"
-            :_id="post._id"
-            :title="post.title"
-            :content="post.content"
-            :username="post.username"
-            :postComment="post.post_comment"
-            :date="post.date"
-            :picture="post.picture"
-            :color="post.color">
-        </post-container>
+        <div v-if="posts.following.length >= 1">
+            <post-container
+                v-for="post in posts.following"
+                :key="post._id"
+                :_id="post._id"
+                :title="post.title"
+                :content="post.content"
+                :username="post.username"
+                :postComment="post.post_comment"
+                :date="post.date"
+                :picture="post.picture"
+                :color="post.color">
+            </post-container>
+        </div>
+        <div v-else>
+            <div class="imagecontainer">
+                <div class="onomatopoeia">crick crick</div>
+                <img class="astronaut-image" src="../../assets/img/astronaut_reading_space_nostars.png" alt="astronaut floating">
+                <div class="loader-text">Looks like you aren't following anyone yet...</div>
+            </div>
+        </div>
     </feed-container>
 </template>
 
@@ -30,3 +39,47 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+.onomatopoeia {
+    color: whitesmoke;
+    font-weight: bold;
+    font-size: 14px;
+    margin-bottom: -10px;
+}
+
+@keyframes floatAnimation {
+  0% {
+    transform: translateY(-5px);
+  }
+  50% {
+    transform: translateY(5px);
+  }
+  100% {
+    transform: translateY(-5px);
+  }
+}
+
+.astronaut-image {
+    width: 175px;
+    height: auto;
+    animation: floatAnimation 4s ease-in-out infinite;
+}
+
+.imagecontainer {
+    margin: 50px auto;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: fit-content;
+}
+
+.loader-text {
+    background-color: whitesmoke;
+    color: rgb(0, 0, 0);
+    padding: 5px 10px;
+    border-radius: 15px;
+    font-weight: bold;
+}
+</style>
