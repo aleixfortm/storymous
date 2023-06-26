41c217c4348a741dab641d9f591f925581113604
aleixfortm <aleix.fort45@gmail.com>
2023-06-26 02:16:32 +0200

    changes

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import store from './store'
 
 export const API_BASE_URL = 'http://192.168.1.44:5000';
 
@@ -11,6 +12,14 @@ if (token && username) {
   axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
   console.log(token)
   console.log(username)
+
+  axios
+    .get(`${API_BASE_URL}/user/${username}`)
+    .then(response => {
+        store.commit('auth/SET_USER_DATA', response.data.userdata);
+    }).catch(error => {
+        console.error('Failed to fetch user data:', error);
+    })
 }
 
 export default axios;
