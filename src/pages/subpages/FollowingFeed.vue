c06ea27142b1084cc24cd5df5e7948af021a5d32
aleixfortm <aleix.fort45@gmail.com>
2023-07-07 02:18:06 +0200

    changes

--- src/pages/subpages/FollowingFeed.vue
+++ src/pages/subpages/FollowingFeed.vue
@@ -1,27 +1,37 @@
 <template>
     <feed-container>
-        <div v-if="posts.following.length >= 1">
-            <post-container
-                v-for="post in posts.following"
-                :key="post._id"
-                :_id="post._id"
-                :title="post.title"
-                :content="post.content"
-                :username="post.username"
-                :postComment="post.comment"
-                :date="post.date"
-                :picture="post.picture"
-                :color="post.color"
-                :feedMode="true">
-            </post-container>
-        </div>
-        <div v-else>
-            <div class="imagecontainer">
-                <div class="onomatopoeia">crick crick</div>
-                <img class="astronaut-image" src="../../assets/img/astronaut_reading_space_nostars.png" alt="astronaut floating">
-                <div class="loader-text">Looks like you aren't following anyone yet...</div>
+        <span v-if="!loading">
+            <div v-if="posts.following.length >= 1">
+                <post-container
+                    v-for="post in posts.following"
+                    :key="post._id"
+                    :_id="post._id"
+                    :title="post.title"
+                    :content="post.content"
+                    :username="post.username"
+                    :postComment="post.comment"
+                    :date="post.date"
+                    :picture="post.picture"
+                    :color="post.color"
+                    :feedMode="true">
+                </post-container>
             </div>
-        </div>
+            <div v-else>
+                <div class="imagecontainer">
+                    <div class="onomatopoeia">crick crick</div>
+                    <img class="astronaut-image" src="../../assets/img/astronaut_reading_space_nostars.png" alt="astronaut floating">
+                    <div class="loader-text">Looks like you aren't following anyone yet...</div>
+                </div>
+            </div>
+        </span>
+        <span v-else class="loader-container">
+            <div class="lds-facebook">
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            <span class="loader-text">Looking for your friends...</span>
+        </span>
     </feed-container>
 </template>
 
@@ -30,7 +40,7 @@ import PostContainer from "../../components/layout/PostContainer.vue";
 import FeedContainer from "@/components/layout/FeedContainer.vue";
 
 export default {
-    props: ["posts"],
+    props: ["posts", "loading"],
     components: {
         PostContainer,
         FeedContainer
@@ -44,6 +54,60 @@ export default {
 </script>
 
 <style scoped>
+.loader-text {
+    background-color: whitesmoke;
+    color: rgb(0, 0, 0);
+    padding: 5px 10px;
+    border-radius: 15px;
+    font-weight: bold;
+}
+
+.loader-container {
+    margin: 40px 0 0 0;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+}
+
+.lds-facebook {
+  display: inline-block;
+  position: relative;
+  width: 80px;
+  height: 80px;
+}
+.lds-facebook div {
+  display: inline-block;
+  position: absolute;
+  left: 8px;
+  width: 16px;
+  background: #fff;
+  animation: lds-facebook 1.2s cubic-bezier(0, 0.5, 0.5, 1) infinite;
+}
+.lds-facebook div:nth-child(1) {
+  left: 8px;
+  animation-delay: -0.24s;
+}
+.lds-facebook div:nth-child(2) {
+  left: 32px;
+  animation-delay: -0.12s;
+}
+.lds-facebook div:nth-child(3) {
+  left: 56px;
+  animation-delay: 0;
+}
+@keyframes lds-facebook {
+  0% {
+    top: 8px;
+    height: 64px;
+  }
+  50%, 100% {
+    top: 24px;
+    height: 32px;
+  }
+}
+
+
 .onomatopoeia {
     color: whitesmoke;
     font-weight: bold;
@@ -79,9 +143,8 @@ export default {
 }
 
 .loader-text {
-    background-color: whitesmoke;
     text-align: center;
-    color: rgb(0, 0, 0);
+    color: black;
     padding: 5px 10px;
     border-radius: 15px;
     font-weight: bold;
