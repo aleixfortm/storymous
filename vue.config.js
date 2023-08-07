1bfa5e041740cf029d4e7c6d29d6a7589c53d16f
aleixfortm <aleix.fort45@gmail.com>
2023-08-07 16:56:54 +0200

    changes to deployment settings

--- vue.config.js
+++ vue.config.js
@@ -3,4 +3,14 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   publicPath: "/",
   outputDir: 'dist',
+  chainWebpack: config => {
+    config.plugin('copy').tap(([options]) => {
+      options.push({
+        from: 'CNAME',
+        to: config.output.get('path'),
+        toType: 'dir'
+      });
+      return [options];
+    });
+  }
 })
